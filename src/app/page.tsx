33edc2782ca04d34e61ebcce3f08ef9779c78cf7
Utkarsh Sharma <ussharma7696@gmail.com>
2024-03-06 23:58:09 +0530

    final

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Footer from "@/components/Footer";
+export default function Home() {
+  return (
+   
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+      </main>
+   
+
+  );
+}
